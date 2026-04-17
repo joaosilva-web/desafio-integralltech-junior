f97237500b2122964d15dfb02f3d67a9f7234742
João Gustavo <joaogustavosilva2001@gmail.com>
2026-04-17 15:47:11 -0300

    refactor: substituir Lombok por getters/setters explícitos em AnaliseResponseDTO

--- src/main/java/com/integralltech/chamados/dto/AnaliseResponseDTO.java
+++ src/main/java/com/integralltech/chamados/dto/AnaliseResponseDTO.java
@@ -2,13 +2,9 @@ package com.integralltech.chamados.dto;
 
 import com.integralltech.chamados.model.enums.Prioridade;
 import com.integralltech.chamados.model.enums.Setor;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
 public class AnaliseResponseDTO {
 
     private Long chamadoId;
@@ -16,4 +12,18 @@ public class AnaliseResponseDTO {
     private Setor setorSugerido;
     private String resumo;
     private LocalDateTime analisadoEm;
+
+    public AnaliseResponseDTO(Long chamadoId, Prioridade prioridadeSugerida, Setor setorSugerido, String resumo, LocalDateTime analisadoEm) {
+        this.chamadoId = chamadoId;
+        this.prioridadeSugerida = prioridadeSugerida;
+        this.setorSugerido = setorSugerido;
+        this.resumo = resumo;
+        this.analisadoEm = analisadoEm;
+    }
+
+    public Long getChamadoId() { return chamadoId; }
+    public Prioridade getPrioridadeSugerida() { return prioridadeSugerida; }
+    public Setor getSetorSugerido() { return setorSugerido; }
+    public String getResumo() { return resumo; }
+    public LocalDateTime getAnalisadoEm() { return analisadoEm; }
 }
